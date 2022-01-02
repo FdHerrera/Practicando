1998a05dc03855e58512967d910510d0a2bdbf66
FdHerrera <soyfdherrera@gmail.com>
2022-01-02 18:24:01 -0300

    Fix UserResponse

--- src/main/java/com/herrera/practicando/dto/response/UserResponse.java
+++ src/main/java/com/herrera/practicando/dto/response/UserResponse.java
@@ -10,5 +10,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class UserResponse {
     private String username;
+    private String firstName;
+    private String lastName;
     private Set<RoleDto> roles;
 }
