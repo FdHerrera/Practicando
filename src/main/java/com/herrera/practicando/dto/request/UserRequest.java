dea2c8e90ac925e87373df6a0d7bd0bdc5fbfb8f
FdHerrera <soyfdherrera@gmail.com>
2022-01-02 18:11:12 -0300

    Forgot one sjjs

--- src/main/java/com/herrera/practicando/dto/request/UserRequest.java
+++ src/main/java/com/herrera/practicando/dto/request/UserRequest.java
@@ -6,6 +6,7 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Validated
@@ -17,6 +18,7 @@ public class UserRequest {
             regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
             message = "Username must be an email, wrong email pattern"
     )
+    @Size(max = 50)
     private String username;
     @NotNull(message = "Firstname can not be null")
     @NotBlank(message = "Firstname can not be empty")
